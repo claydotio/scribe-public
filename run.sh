39cf53c9e55d45c4bf596115b89ce7b9509bc617
Zolmeister <zolikahan@gmail.com>
2014-11-01 14:46:38 -0700

    init

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat << EOF > /tmp/config.json
+{
+  "network": {
+    "servers": [ "$LOGSTASH_SERVER" ],
+    "ssl certificate": "/opt/certs/logstash-forwarder.crt",
+    "ssl key": "/opt/certs/logstash-forwarder.key",
+    "ssl ca": "/opt/certs/logstash-forwarder.crt",
+    "timeout": 15
+  },
+  "files": [
+    {
+      "paths": [ "/var/log/clay/*.log" ],
+      "fields": { "type": "stdin" }
+    }
+  ]
+}
+EOF
+/opt/logstash-forwarder -config /tmp/config.json
